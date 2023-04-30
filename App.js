7f46cf58e99dd3adbbcb12d4e9d1bb979de83458
jvchiu131 <85332493+jvchiu131@users.noreply.github.com>
2023-04-30 15:09:48 +0800

    added Dashboard Screen and Login Screen for initial results

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './screens/components/LoginScreen';
-import DashScreen from './screens/DashScreen';
+import DashScreen from './screens/components/DashScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
